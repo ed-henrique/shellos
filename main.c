90415e93e2a91eb242351f6e9425c13792cce077
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-12-27 15:20:43 -0400

    :sparkles: execute commands without pipe

--- main.c
+++ main.c
@@ -2,40 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 void runShell(FILE* fp) {
-    const int argLimit = 25;
     int commandMaxLength = 512;
     char* command = (char*) malloc(commandMaxLength * sizeof(char));
 
+    printf("> ");
+
     while (fgets(command, commandMaxLength, fp) != NULL) {
-        int i = 0;
-        char* path;
-        char* arguments[argLimit];
         command[strcspn(command, "\r\n")] = 0;
-        char* token = strtok(command, " ");
 
-        asprintf(&path, "/bin/%s", token);
-
-        while (token != NULL) {
-            token = strtok(NULL, " ");
-            asprintf(&arguments[i++], "%s", token);
+        if (!strcmp(command, "exit") || !strcmp(command, "quit")) {
+            exit(0);
         }
 
-        if (fork() == 0) {
-            if (i > 1) {
-                execl(path, command, arguments, NULL);
-            } else {
-                execl(path, command, NULL);
+        if (!strlen(command)) {
+            continue;
+        }
+
+        int argLimit = 0;
+
+        for (int i = 0; i < strlen(command); i++) {
+            if (command[i] == ' ') {
+                argLimit++;
             }
         }
 
-        printf("\n");
-        free(path);
+        argLimit++;
+
+        int i = 0;
+        char** arguments = (char**) malloc(sizeof(char*) * (argLimit + 1));
+        char* token = strtok(command, " ");
+
+        do {
+            asprintf(&arguments[i++], "%s", token);
+            token = strtok(NULL, " ");
+        } while (token != NULL);
+
+        arguments[argLimit] = NULL;
+
+        pid_t pid = fork();
+
+        if (pid == -1) {
+            fprintf(stderr, "Could not fork child!\n");
+            exit(1);
+        } else if (pid == 0) {
+            if (execvp(arguments[0], arguments) < 0) {
+                fprintf(stderr, "Could not execute command!\n");
+                exit(1);
+            }
+        } else {
+            wait(NULL);
+        }
+
+        printf("\n> ");
         free(token);
+        free(arguments);
     }
 
     free(command);
+    printf("\n");
     exit(1);
 }
 
